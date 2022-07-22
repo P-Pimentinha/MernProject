e05ab40417a27a45586ede3322b00e0d39047f07
Pedro Pimentinha <pedro.pimentinha18@gmail.com>
2022-07-22 09:41:50 +0200

    created layout for main page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,31 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Dashboard, Landing, Error, Register } from './pages';
+import { Landing, Error, Register, ProtectedRoute } from './pages';
+import {
+  AddJob,
+  AllJobs,
+  Profile,
+  SharedLayout,
+  Stats,
+} from './pages/dashboard';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Dashboard />}></Route>
+          <Route
+            path='/'
+            element={
+              <ProtectedRoute>
+                <SharedLayout />
+              </ProtectedRoute>
+            }
+          >
+            <Route index element={<Stats />} />
+            <Route path='profile' element={<Profile />} />
+            <Route path='add-job' element={<AddJob />} />
+            <Route path='all-jobs' element={<AllJobs />} />
+          </Route>
           <Route path='/register' element={<Register />}></Route>
           <Route path='/landing' element={<Landing />}></Route>
           <Route path='*' element={<Error />}></Route>
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,5 +1,8 @@
 import Logo from './logo';
 import Form from './FormRow';
 import Alert from './Alert';
+import Navbar from './Navbar';
+import BigSidebar from './BigSidebar';
+import SmallSidebar from './SmallSidebar';
 
-export { Logo, Form, Alert };
+export { Logo, Form, Alert, Navbar, BigSidebar, SmallSidebar };
